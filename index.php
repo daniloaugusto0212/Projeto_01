962704b3f5c72792ba4dbe06911031db6f873355
daniloaugusto0212 <daniloaugusto0212@hotmail.com>
2020-02-04 20:46:02 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Projeto 01</title>
+    <link href="estilo/style.css" rel="stylesheet"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="palavras-chave,do,meu,site">
+    <meta name="description" content="Descrição do meu site">
+    <meta charset="UTF-8">   
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">    
+</head>
+<body>
+
+    <header>
+        <div class="center">
+            <div class="logo">Logomarca</div><!--logo-->
+            <nav class="desktop">
+                <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+                </ul>        
+            </nav>
+            <nav class="mobile">
+                <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+                </ul>        
+            </nav>
+        </div><!--center-->
+
+    </header>
+
+    <section class="banner-principal">
+        <div class="center">
+            <form>
+                <h2>Qual o seu melhor e-mail?</h2>
+                <input type="email" name="email" required />
+                <input type="submit" name="acao" value="Cadastrar!">
+            </form> <!--banner-principal-->
+        </div><!--center-->
+    </section>
+    
+    <section class="descricao-autor">
+        <div class="center" >
+            <div class="w50">
+                <h2>Danilo A. Pacheco</h2>
+                <p>Lorem Ipsum é simplesmente um texto fictício da indústria tipográfica e de impressão. Lorem Ipsum é o texto fictício padrão do setor desde os anos 1500, quando uma impressora desconhecida pegou uma galera do tipo e a mexeu para fazer um livro de amostras do tipo. Ele sobreviveu não apenas cinco séculos, mas também o salto para a composição eletrônica, permanecendo essencialmente inalterado. Foi popularizado na década de 1960 com o lançamento de folhas de Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como o Aldus PageMaker, incluindo versões do Lorem Ipsum.</p>
+                <p>Lorem Ipsum é simplesmente um texto fictício da indústria tipográfica e de impressão. Lorem Ipsum é o texto fictício padrão do setor desde os anos 1500, quando uma impressora desconhecida pegou uma galera do tipo e a mexeu para fazer um livro de amostras do tipo. Ele sobreviveu não apenas cinco séculos, mas também o salto para a composição eletrônica, permanecendo essencialmente inalterado. Foi popularizado na década de 1960 com o lançamento de folhas de Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como o Aldus PageMaker, incluindo versões do Lorem Ipsum.</p>
+            </div><!--w50-->
+            <div class="w50">
+                <img src="images/foto.jpg" alt="foto">
+            </div><!--w50-->  
+        </div><!--center-->      
+    </section><!--descricao-autor-->
+
+    <section class="especialidades">        
+        <div class="center">
+            <h2 class="title">Especialidades</h2>
+            <div class="box=especialidade">
+                <h3>icone1</h3>
+                <h3>CSS3</h3>
+                <p>Lorem Ipsum é simplesmente um texto fictício da indústria tipográfica e de impressão. Lorem Ipsum é o texto fictício padrão do setor desde os anos 1500, quando uma impressora desconhecida pegou uma galera do tipo </p>
+            </div><!--box-especialidade-->
+            <div class="box=especialidade">
+                <h3>icone2</h3>
+                <h3>HTML5</h3>
+                <p>Lorem Ipsum é simplesmente um texto fictício da indústria tipográfica e de impressão. Lorem Ipsum é o texto fictício padrão do setor desde os anos 1500, quando uma impressora desconhecida pegou uma galera do tipo </p>
+            </div><!--box-especialidade-->
+            <div class="box=especialidade">
+                <h3>icone3</h3>
+                <h3>JavaScript</h3>
+                <p>Lorem Ipsum é simplesmente um texto fictício da indústria tipográfica e de impressão. Lorem Ipsum é o texto fictício padrão do setor desde os anos 1500, quando uma impressora desconhecida pegou uma galera do tipo </p>
+            </div><!--box-especialidade-->
+        </div><!--center--> 
+    </section><!--especialidades-->
+
+    <section class="extras">
+
+        <div class="center">
+            <div class="w50">
+                <h2 class="title">Depoimentos</h2>
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed the eiusmod tempor incidid ut labore et dolore magna aliqua. Isso significa que, no mínimo, veniam, exigiu o exercício da nossa vontade, não é por isso que é excomodado. velit esse cillum dolore eu fugi nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. "</p>
+                    <p class="nome-autor">Loren Ipsum</p>
+                </div><!--depoimento-single-->
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed the eiusmod tempor incidid ut labore et dolore magna aliqua. Isso significa que, no mínimo, veniam, exigiu o exercício da nossa vontade, não é por isso que é excomodado. velit esse cillum dolore eu fugi nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. "</p>
+                    <p class="nome-autor">Loren Ipsum</p>
+                </div><!--depoimento-single-->
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed the eiusmod tempor incidid ut labore et dolore magna aliqua. Isso significa que, no mínimo, veniam, exigiu o exercício da nossa vontade, não é por isso que é excomodado. velit esse cillum dolore eu fugi nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. "</p>
+                    <p class="nome-autor">Loren Ipsum</p>
+                </div><!--depoimento-single-->
+            </div><!--w50-->
+            <div class="w50">
+                <h2 class="title">Serviços</h2>
+                <div class="servicos">
+                <ul>
+                    <li>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed the eiusmod tempor incidid ut labore et dolore magna aliqua. Isso significa que, no mínimo, veniam, exigiu o exercício da nossa vontade, não é por isso que é excomodado. velit esse cillum dolore eu fugi nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. "</li>
+                    <li>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed the eiusmod tempor incidid ut labore et dolore magna aliqua. Isso significa que, no mínimo, veniam, exigiu o exercício da nossa vontade, não é por isso que é excomodado. velit esse cillum dolore eu fugi nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. "</li>
+                    <li>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed the eiusmod tempor incidid ut labore et dolore magna aliqua. Isso significa que, no mínimo, veniam, exigiu o exercício da nossa vontade, não é por isso que é excomodado. velit esse cillum dolore eu fugi nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. "</li>
+                </ul>
+                </div><!--servicos-->
+                </div><!--depoimento-single-->
+            </div><!--w50-->
+        </div><!--center-->
+    
+    </section><!--extras-->
+
+    <footer>
+        <div class="center">
+            <p>Todo os direitos reservados</p>
+        </div><!--center-->
+    </footer>
+</body>
+</html>
+
