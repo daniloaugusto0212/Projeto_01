8278e296f08ce898791bd827b33b38242b3e8326
daniloaugusto0212 <daniloaugusto0212@hotmail.com>
2020-02-05 22:00:35 -0300

    Finalizando CSS

--- index.php
+++ index.php
@@ -90,8 +90,8 @@
     <section class="extras">
 
         <div class="center">
-            <div class="w50 left ">
-                <h2 class="title">Depoimentos</h2>
+            <div class="w50 left depoimentos-caontainer">
+                <h2 class="title">Depoimentos dos nossos clientes</h2>
                 <div class="depoimento-single">
                     <p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed the eiusmod tempor incidid ut labore et dolore magna aliqua. Isso significa que, no mínimo, veniam, exigiu o exercício da nossa vontade, não é por isso que é excomodado. velit esse cillum dolore eu fugi nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. "</p>
                     <p class="nome-autor">Loren Ipsum</p>
@@ -105,13 +105,13 @@
                     <p class="nome-autor">Loren Ipsum</p>
                 </div><!--depoimento-single-->
             </div><!--w50 left-->
-            <div class="w50 left">
+            <div class="w50 left servicos-container">
                 <h2 class="title">Serviços</h2>
                 <div class="servicos">
                 <ul>
-                    <li>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed the eiusmod tempor incidid ut labore et dolore magna aliqua. Isso significa que, no mínimo, veniam, exigiu o exercício da nossa vontade, não é por isso que é excomodado. velit esse cillum dolore eu fugi nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. "</li>
-                    <li>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed the eiusmod tempor incidid ut labore et dolore magna aliqua. Isso significa que, no mínimo, veniam, exigiu o exercício da nossa vontade, não é por isso que é excomodado. velit esse cillum dolore eu fugi nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. "</li>
-                    <li>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed the eiusmod tempor incidid ut labore et dolore magna aliqua. Isso significa que, no mínimo, veniam, exigiu o exercício da nossa vontade, não é por isso que é excomodado. velit esse cillum dolore eu fugi nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. "</li>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed the eiusmod tempor incidid ut labore et dolore magna aliqua. Isso significa que, no mínimo, veniam, exigiu o exercício da nossa vontade, não é por isso que é excomodado. velit esse cillum dolore eu fugi nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. </li>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed the eiusmod tempor incidid ut labore et dolore magna aliqua. Isso significa que, no mínimo, veniam, exigiu o exercício da nossa vontade, não é por isso que é excomodado. velit esse cillum dolore eu fugi nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. </li>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed the eiusmod tempor incidid ut labore et dolore magna aliqua. Isso significa que, no mínimo, veniam, exigiu o exercício da nossa vontade, não é por isso que é excomodado. velit esse cillum dolore eu fugi nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. </li>
                 </ul>
                 </div><!--servicos-->
                 </div><!--depoimento-single-->
