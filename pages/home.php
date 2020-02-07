e093c4b1faa2ed7225f8b81cc30c12edaaae0e90
Danilo Augusto <daniloaugusto0212@hotmail.com>
2020-02-07 12:45:45 -0300

    Adicionando slides

--- pages/home.php
+++ pages/home.php
@@ -1,13 +1,16 @@
-<section class="banner-principal">
-    <div class="overlay"></div><!--overlay-->
-    <div class="center">
-        <form>
-            <h2>Qual o seu melhor e-mail?</h2>
-            <input type="email" name="email" required />
-            <input type="submit" name="acao" value="Cadastrar!">
-        </form> <!--banner-principal-->
-    </div><!--center-->
-</section><!--banner-principal-->
+<section class="banner-container">
+    <div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/bg-form.jpg');" class="banner-single"></div><!--banner-single-->
+    <div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/bg-form2.jpg');" class="banner-single"></div><!--banner-single-->
+    <div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/bg-form3.jpg');" class="banner-single"></div><!--banner-single-->
+        <div class="overlay"></div><!--overlay-->
+        <div class="center">
+            <form>
+                <h2>Qual o seu melhor e-mail?</h2>
+                <input type="email" name="email" required />
+                <input type="submit" name="acao" value="Cadastrar!">
+            </form> <!--banner-principal-->
+        </div><!--center-->
+</section><!--banner-container-->
 
 <section class="descricao-autor">
     <div class="center" >
