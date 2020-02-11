09557d413bd8dcb0fc35e7cc00e097c1c2ac0ff1
daniloaugusto0212 <daniloaugusto0212@hotmail.com>
2020-02-11 20:54:25 -0300

    Painel de logout

--- painel/main.php
+++ painel/main.php
@@ -1,4 +1,35 @@
+<?php
+    if(isset($_GET['logout'])){
+        Painel::logout();
+    }
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Painel de controle</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="<?php echo INCLUDE_PATH; ?>estilo/css/all.css" rel="stylesheet"> <!--load all styles -->
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,700&display=swap" rel="stylesheet">    
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH_PAINEL ?>css/style.css">
+</head>
+<body>
+<div class="menu">
+    <?php
+    
+    ?>
+</div>
+<header>
+    <div class="center">
+        <div class="logout">
+            <a href="<?php echo INCLUDE_PATH_PAINEL ?>?logout"><i class="fas fa-sign-out-alt"></i></a>
+        </div><!--logout-->
+        <div class="clear"></div><!--clear-->
+    </div><!--center-->
+</header>
 
+</body>
+</html>
 
-<h2>Bem vindo ao painel admin</h2>
 
