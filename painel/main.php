22c7b4e424546f06959da0c9a7054eea577ccb16
Danilo Augusto <daniloaugusto0212@hotmail.com>
2020-02-12 10:35:26 -0300

    Menu painel

--- painel/main.php
+++ painel/main.php
@@ -17,7 +17,7 @@
 <body>
 <div class="menu">
     <div class="menu-wraper">
-    <div class="box-usuario">
+        <div class="box-usuario">
         <?php
             if($_SESSION['img'] == ''){              
         ?>
@@ -33,9 +33,24 @@
                 <p><?php echo $_SESSION['nome']; ?></p>
                 <p><?php echo pegaCargo($_SESSION['cargo']); ?></p>
             </div><!--nome-usuario-->        
-    </div><!--box-usuario-->
+        </div><!--box-usuario-->
+    <div class="items-menu">
+        <h2>Cadastro</h2>
+        <a href="">Cadastrar Depoimento</a>
+        <a href="">Cadastrar Serviço</a>
+        <a href="">Cadastrar Slides</a>
+        <h2>Gestão</h2>
+        <a href="">Listar Depoimentos</a>
+        <a href="">Listar Serviços</a>
+        <h2>Administração do Painel</h2>
+        <a href="">Editar Usuário</a>
+        <a href="">Adicionar Usuário</a>
+        <h2>Configuração Geral</h2>
+        <a href="">Editar</a>
+    </div><!--items-menu-->
     </div><!--menu-wraper-->
 </div><!--menu-->
+
 <header>
     <div class="center">
         <div class="menu-btn">
@@ -47,22 +62,44 @@
         <div class="clear"></div><!--clear-->
     </div><!--center-->
 </header>
-    <div class="content">
-        <div class="box-content left w100">
+<div class="content">
 
-        </div><!--box-content-->
-        <!--<div class="box-content left w100">
+    <div class="box-content left w100">
+        <h2><i class="fa fa-home"> Painel de Controle - <?php echo $nomeEmpresa ?></i></h2>
+            <div class="box-metricas">
+                <div class="box-metrica-single">
+                    <div class="box-metrica-wraper">
+                        <h2>Usuários Online</h2>
+                        <p>10</p>
+                    </div><!--box-metrica-wraper-->
+                </div><!--box-metrica-single-->
+                <div class="box-metrica-single">
+                    <div class="box-metrica-wraper">
+                        <h2>Total de Visitas</h2>
+                        <p>100</p>
+                    </div><!--box-metrica-wraper-->
+                </div><!--box-metrica-single-->
+                <div class="box-metrica-single">
+                    <div class="box-metrica-wraper">
+                        <h2>Visitas Hoje</h2>
+                        <p>3</p>
+                    </div><!--box-metrica-wraper-->
+                </div><!--box-metrica-single-->
+            </div><!--box-metricas-->
 
-        </div>
-        <div class="box-content left w50">
+    </div><!--box-content-->
+    <!--<div class="box-content left w100">
 
-        </div>
-        <div class="box-content right w50">
+    </div>
+    <div class="box-content left w50">
 
-        </div>
-        -->
-        <div class="clear"></div>
-    </div><!--content-->
+    </div>
+    <div class="box-content right w50">
+
+    </div>
+    -->
+    <div class="clear"></div>
+</div><!--content-->
 <script src="<?php echo INCLUDE_PATH ?>js/jquery.js"></script>   
 <script src="<?php echo INCLUDE_PATH_PAINEL ?>js/main.js"></script>
 </body>
