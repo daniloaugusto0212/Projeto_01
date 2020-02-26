2b49ecd295fcdc8289fa8562e666fd253df11f49
Danilo Augusto <daniloaugusto0212@hotmail.com>
2020-02-26 19:09:52 -0300

    Cadastro de slides

--- painel/pages/cadastrar-slides.php
+++ painel/pages/cadastrar-slides.php
@@ -1,10 +1,6 @@
 
-<?php
-    verificaPermissaoPagina(2);
-    
-?>
 <div class="box-content">
-    <h2><i class="fas fa-user-plus"></i> Adicionar Slides</h2>
+    <h2><i class="fas fa-user-plus"></i> Cadastrar Slides</h2>
 
     <form  method="post" enctype="multipart/form-data"> <!--enctype="multipart/form-data" para funcionar o upload de imagens-->
 
@@ -13,58 +9,30 @@
                 $nome = $_POST['nome'];                
                 $imagem = $_FILES['imagem'];
                 
-                if ($login == '') {                    
-                    Painel::alert('erro',' O nome está vazio!');                
-                }else if($imagem['name'] ==''){
-                    Painel::alert('erro',' A imagem precisa estar selecionada!');
+                if ($nome == '') {                    
+                    Painel::alert('erro',' O campo nome não pode ficar vazio!');              
                 }else{
-                    //Podemos cadastrar!
-                    if ($cargo >= $_SESSION['cargo']) {                        
-                        Painel::alert('erro',' Você precisa selecionar um cargo maior que o seu!');                        
-                    }else if(Painel::imagemValida($imagem) == false){
-                        Painel::alert('erro',' O formato especificado não está correto!'); 
-                    }else if(usuario::userExists($login)){
-                        Painel::alert('erro',' O login já existe, selecione outro, por favor!'); 
+                    //Podemos cadastrar!                                     
+                    if(Painel::imagemValida($imagem) == false){
+                        Painel::alert('erro',' O formato especificado não está correto!');                   
                     }else{
-                        //Apenas cadastrar no banco de dados!
-                        $usuario = new Usuario();
+                        //Apenas cadastrar no banco de dados!                        
                         $imagem = Painel::uploadFile($imagem);
-                        $usuario->cadastrarUsuario($login,$senha,$imagem,$nome,$cargo);
-
-                        Painel::alert('sucesso', ' O cadastro do usuário '.$login.' foi feito com sucesso!');
+                        $arr = ['nome'=>$nome,'slide'=>$imagem,'order_id'=>'0', 'nome_tabela'=>'tb_site.slides'];
+                        Painel::insert($arr);   
+                        Painel::alert('sucesso', ' O cadastro do slide foi realizado com sucesso!');
                     }
                 }                
                 
             }
         ?>
-
+        
         <div class="form-group">
-            <label>Login: </label>
-            <input type="text" name="login">
-        </div><!--form-group-->
-
-        <div class="form-group">
-            <label>Nome: </label>
+            <label>Nome do slide: </label>
             <input type="text" name="nome">
         </div><!--form-group-->
 
-        <div class="form-group">
-            <label>Senha: </label>
-            <input type="password" name="password">
-        </div><!--form-group-->
-
-        <div class="form-group">
-            <label>Cargo: </label>
-            <select  name="cargo">
-                <?php
-                    foreach (Painel::$cargos as $key => $value) {
-                        if($key < $_SESSION['cargo']) echo '<option value="'.$key.'">'.$value.'</option>';
-                    }
-                ?>
-            </select>
-        </div><!--form-group-->
-
-
+        
         <div class="form-group">
             <label>Imagem</label>
             <input type="file" name="imagem" >            
